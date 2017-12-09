5d556331f8fd141e2a07697b92dc270e742efe5c
johnny850807 <johnny850807@gmail.com>
2017-12-10 02:00:49 +0800

    pull

--- 英英單字大對決/英英單字大對決/src/ui/GameViewImp.java
+++ 英英單字大對決/英英單字大對決/src/ui/GameViewImp.java
@@ -8,15 +8,11 @@ import java.util.List;
 
 import javax.swing.JPanel;
 
-import org.omg.CORBA.PRIVATE_MEMBER;
-
 import controller.EEFighter;
 import model.Question;
 import model.sprite.GameMap;
 import model.sprite.IGameStartView;
-import model.sprite.LetterPool;
 import model.sprite.Sprite;
-import model.sprite.SpriteName;
 import model.sprite.Sprite.Direction;
 import model.sprite.Sprite.Status;
 
