1578567872495420cc95ab51197d7fe8658ea4ee
xuan1280 <xuan1280@gmail.com>
2017-12-15 23:53:00 +0800

    next question clean player information

--- 英英單字大對決/英英單字大對決/src/model/sprite/LetterManager.java
+++ 英英單字大對決/英英單字大對決/src/model/sprite/LetterManager.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import controller.EEFighter;
+
 public class LetterManager {
 	
 	private List<Sprite> letters = new ArrayList<>();
