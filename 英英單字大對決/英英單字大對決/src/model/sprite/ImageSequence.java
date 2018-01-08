f89b8ff4585403f44bbfd103e21849e831271620
johnny850807 <johnny850807@gmail.com>
2018-01-09 02:32:46 +0800

    嗚嗚

--- 英英單字大對決/英英單字大對決/src/model/sprite/ImageSequence.java
+++ 英英單字大對決/英英單字大對決/src/model/sprite/ImageSequence.java
@@ -6,6 +6,7 @@ import java.io.File;
 import javax.imageio.ImageIO;
 
 public class ImageSequence {
+	private static final int BASE = 8;
 	private int picAmount;
 	private Image[] images;
 	private int returnNumber = 0;
@@ -17,8 +18,8 @@ public class ImageSequence {
 	}
 
 	public Image nextImage() {
-		returnNumber++;
-		if (returnNumber % 4 == 0)
+		returnNumber = returnNumber + 1 == BASE ? 0 : returnNumber + 1 ;
+		if (returnNumber == BASE - 1)
 			index++;
 		return images[index % picAmount];
 	}
